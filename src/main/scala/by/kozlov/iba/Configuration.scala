36436d31935e9262eb4753de1c73ea9f8f76df1a
Kazlou_KA <kirillkozlou@yandex.ru>
2021-08-05 21:06:00 +0300

    Some fixes in configuration were made

--- src/main/scala/by/kozlov/iba/Configuration.scala
+++ src/main/scala/by/kozlov/iba/Configuration.scala
@@ -1,24 +1,28 @@
 package by.kozlov.iba
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 
 object Configuration {
   val ss = SparkSession
     .builder()
+    .appName("ibaTask")
+    .config("option", "option`1")
     .getOrCreate()
-  val inputDB = ss.read.format("jdbc")
-    .option("username", sys.env("dbUsername"))
-    .option("password", sys.env("dbPassword"))
-    .option("driver", "com.ibm.db2.jcc.DB2Driver")
-//    .option("dbtable","(SELECT * FROM PRT00338.product_record WHERE ((MOD(year, product_id) >= 1 and MOD(year, product_id) < 3000)")
-//    .option("partitionColumn","mod")
-//    .option("lowerBound","1")
-//    .option("upperBound","3000")
-//    .option("numPartitions","15")
-//    .option("partitionColumn","year")
-//    .option("lowerBound","201")
-//    .option("upperBound","2019")
-    .option("url", "jdbc:db2://dashdb-txn-sbox-yp-dal09-14.services.dal.bluemix.net:50000/BLUDB:user=" + sys.env("dbUsername") + ";PWD=" + sys.env("dbPassword") + ";Security=SSL;")
-    .option("dbtable", "PRT00338.product_record")
+  val inputDB = ss.read.format(sys.env("source"))
+    .option("username", ss.sparkContext.getConf.get("spark.myvariables.dbUsername"))
+    .option("password", ss.sparkContext.getConf.get("spark.myvariables.dbPassword"))
+    .option("driver", ss.sparkContext.getConf.get("spark.myvariables.driver"))
+    .option("fetchsize","2000")
+    .option("numPartitions", ss.sparkContext.getConf.get("spark.myvariables.numPartitions"))
+    .option("partitionColumn", ss.sparkContext.getConf.get("spark.myvariables.PartitionColumn"))
+    .option("lowerBound", ss.sparkContext.getConf.get("spark.myvariables.lowerBound"))
+    .option("upperBound", ss.sparkContext.getConf.get("spark.myvariables.upperBound"))
+    .option("url", ss.sparkContext.getConf.get("spark.myvariables.url"))
+    .option("dbtable", ss.sparkContext.getConf.get("spark.myvariables.dbtable"))
     .load()
+
+  def main(args: Array[String]): Unit = {
+    ss.sparkContext.getConf.get("option")
+  }
 }
